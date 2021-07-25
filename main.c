6662fa022162948c5b0b8711e086b4a87ba77c9a
kapil-panchal <kapil.panchal@nouvelletechnology.com.au>
2021-07-25 14:20:51 +1000

    Removed titlebar and gtk_window_set_decorated-->FALSE

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ static void activate(GtkApplication* app, gpointer user_data)
   gtk_container_add (GTK_CONTAINER (window), label);
   gtk_window_set_title(GTK_WINDOW (window), "Welcome to GNOME");
   gtk_window_set_default_size(GTK_WINDOW (window), 200, 100);
+  gtk_window_set_decorated(GTKWINDOW (window), FALSE); 
   gtk_widget_show_all(window);
 }
 
@@ -18,7 +19,7 @@ int main(int argc, char **argv)
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new(NULL, G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new("com.nouvelletechnology.example", G_APPLICATION_FLAGS_NONE);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
